92c97385207f9627e0ea6069eb9cecc99a43ea28
CodeBleck <belder_2007@yahoo.fr>
2018-01-12 12:47:00 +0100

    Webproject

---
+++ navmenu.php
@@ -0,0 +1,80 @@
+<nav class="menu">
+					<div class="icon-list nav-tabs">
+						<div class="row" >
+							<div class="col-md-12">
+							<div>
+								<li >
+									<a  data-parent="#accordion" href="#vente" data-toggle="collapse" ><i><img src="images/icons/ventes.png"></i><span>VENTES</span>  <b class="caret"></b>
+									</a>
+								</li>
+							</div>
+							<div class="collapse-in in col-md-12" id="vente">
+									<ul >
+										<li><a href="#">Electronique</a></li>
+										<li><a href="#">Immobiliers</a></li>
+										<li><a href="#">Vestimentaires</a></li>
+										<li><a href="#">Cosmetiques</a></li>
+										<li><a href="#">Bijoux</a></li>
+									</ul>
+							</div>
+						</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+							<div>
+								<li  >
+									<a data-parent="#accordion" href="#location" data-toggle="collapse" ><i><img src="images/icons/locations.png"></i><span>LOCATIONS</span>  <b class="caret"></b>
+									</a>
+								</li>
+							</div>
+							<div class="collpse-in col-md-12" id="location">
+								<ul >
+									<li><a href="#">Electronique</a></li>
+									<li><a href="#">Immobiliers</a></li>
+									<li><a href="#">Vestimentaires</a></li>
+									<li><a href="#">Cosmetiques</a></li>
+									<li><a href="#">Bijoux</a></li>
+								</ul>
+							</div>
+						</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+							<div>
+								<li >
+									<a data-parent="#accordion" href="#emploi" data-toggle="collapse" ><i><img src="images/icons/emplois.png"></i><span>EMPLOIS</span>  <b class="caret"></b>
+									</a>
+								</li>
+							</div>
+							<div class="collpse-in col-md-12" id="emploi">
+								<ul >
+									<li><a href="#">Electronique</a></li>
+									<li><a href="#">Immobiliers</a></li>
+									<li><a href="#">Vestimentaires</a></li>
+									<li><a href="#">Cosmetiques</a></li>
+									<li><a href="#">Bijoux</a></li>
+								</ul>
+							</div>
+						</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+							<div>
+							<li  >
+								<a data-parent="#accordion" href="#formation" data-toggle="collapse"><i><img src="images/icons/formations.png"></i><span>FORMATIONS</span>  <b class="caret"></b>
+								</a>
+							</li>
+							</div>
+							<div  class="collpse-in col-md-12" id="formation">
+								<ul >
+									<li><a href="#">Electronique</a></li>
+									<li><a href="#">Immobiliers</a></li>
+									<li><a href="#">Vestimentaires</a></li>
+									<li><a href="#">Cosmetiques</a></li>
+									<li><a href="#">Bijoux</a></li>
+								</ul>
+							</div>
+							</div>
+						</div>
+					</div>
+				</nav>
