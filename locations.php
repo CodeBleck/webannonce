444b99158ee1863d8b248d0b19676f351fd818ed
guesson <belder_2007@yahoo.fr>
2018-01-22 13:54:21 +0100

    modifications de la sections location

--- locations.php
+++ locations.php
@@ -569,275 +569,210 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						<a class="w3ls-ads" href="all-classifieds.php">View all Ads</a>
 					</div>
 					<div class="resp-tabs-container hor_1">
-						<span class="active total" style="display:block;" data-toggle="modal" data-target="#myModal"><strong>All USA</strong> (Select your city to see local ads)</span>
+						<span class="active total" style="display:block;" data-toggle="modal" data-target="#myModal"><strong>CAMEROUN</strong> (Selectionner votre ville)</span>
 						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat1.png" title="image" alt="" />
+							<div>
+							<h3>MAISONS</h3> <a href="all-classifieds.php">View all Ads</a>
+							<hr>
+							<figure class="category row">
+
+								<div class="category-img col-md-3">
+									<img src="images/r4.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="category-info">
-									<h4>Maisons</h4>
-									<span>5,12,850 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
+								
+								<div class="category-img col-md-3">
+									<img src="images/r5.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="clearfix"></div>
-							</div>
+								
+								<div class="category-img col-md-3">
+									<img src="images/r6.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+
+								<div class="category-img col-md-3">
+									<img src="images/r7.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+								
+								
+							</figure>
+
 							<div class="sub-categories">
 								<ul>
-									<li><a href="mobiles.php">mobile phones</a></li>
-									<li><a href="mobiles.php">Tablets</a></li>
-									<li><a href="mobiles.php">Accessories</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Appartements</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Studios</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Chambre</a></li>
 								</ul>
 							</div>
 						</div>
+						</div>
+
 						<div>
-							<div class="category">
-								<div class="category-img">
-                                    <img src="images/cat3.png" title="image" alt="" />
+							<div>
+							<div>
+							<h3>VEHICULES</h3> <a href="all-classifieds.php">View all Ads</a>
+							<hr>
+							<figure class="category row">
+
+								<div class="category-img col-md-3">
+									<img src="images/c23.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="category-info">
-									<h4>Vehicules</h4>
-									<span>2,01,850 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
+								
+								<div class="category-img col-md-3">
+									<img src="images/c4.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="clearfix"></div>
-							</div>
+								
+								<div class="category-img col-md-3">
+									<img src="images/c8.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+
+								<div class="category-img col-md-3">
+									<img src="images/bk11.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+								
+								
+							</figure>
+
 							<div class="sub-categories">
 								<ul>
-									<li><a href="electronics-appliances.php">Computers & accessories</a></li>
-									<li><a href="electronics-appliances.php">Tv - video - audio</a></li>
-									<li><a href="electronics-appliances.php">Cameras & accessories</a></li>
-									<li><a href="electronics-appliances.php">Games & Entertainment</a></li>
-									<li><a href="electronics-appliances.php">Fridge - AC - Washing Machine</a></li>
-									<li><a href="electronics-appliances.php">Kitchen & Other Appliances</a></li>
+									<li class="btn btn-info"><a href="bikes.php">Motos</a></li>
+									<li class="btn btn-info"><a href="cars.php">Camions</a></li>
+									<li class="btn btn-info"><a href="cars-.php">Voitures</a></li>
+									<li class="btn btn-info"><a href="cars.php">Bus</a></li>
 								</ul>
 							</div>
 						</div>
+					</div>
+				</div>
+						
+					<div>
 						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat3.png" title="image" alt="" />
+							<div>
+							<h3>Terrains</h3> <a href="all-classifieds.php">View all Ads</a>
+							<hr>
+							<figure class="category row">
+
+								<div class="category-img col-md-3">
+									<img src="images/t1.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="category-info">
-									<h4>Terrains</h4>
-									<span>1,98,080 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
+								
+								<div class="category-img col-md-3">
+									<img src="images/t4.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="clearfix"></div>
-							</div>
+								
+								<div class="category-img col-md-3">
+									<img src="images/t2.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+
+								<div class="category-img col-md-3">
+									<img src="images/t3.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+								
+								
+							</figure>
+
 							<div class="sub-categories">
 								<ul>
-									<li><a href="cars.php">Commercial Vehicles</a></li>
-									<li><a href="cars.php">Other Vehicles</a></li>
-									<li><a href="cars.php">Spare Parts</a></li>
+									<li class="btn btn-info"><a href="bikes.php">Ventes</a></li>
+									<li class="btn btn-info"><a href="cars.php">Locations</a></li>
 								</ul>
 							</div>
 						</div>
+					</div>
+				</div>
+						
 						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat4.png" title="image" alt="" />
+						<div>
+							<div>
+							<h3>Salles</h3> <a href="all-classifieds.php">View all Ads</a>
+							<hr>
+							<figure class="category row">
+
+								<div class="category-img col-md-3">
+									<img src="images/salles/s1.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="category-info">
-									<h4>Salles</h4>
-									<span>6,17,568 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
+								
+								<div class="category-img col-md-3">
+									<img src="images/salles/s2.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="clearfix"></div>
-							</div>
+								
+								<div class="category-img col-md-3">
+									<img src="images/salles/s3.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+
+								<div class="category-img col-md-3">
+									<img src="images/salles/s4.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+								
+								
+							</figure>
+
 							<div class="sub-categories">
 								<ul>
-									<li><a href="bikes.php">Motorcycles</a></li>
-									<li><a href="bikes.php">Scooters</a></li>
-									<li><a href="bikes.php">Bicycles</a></li>
-									<li><a href="bikes.php">Spare Parts</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Locations Complete</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Decorations</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Locations Simple</a></li>
 								</ul>
 							</div>
 						</div>
+					</div>
+				</div>
+
 						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat5.png" title="image" alt="" />
+						<div>
+							<div>
+							<h3>Chaises</h3> <a href="all-classifieds.php">View all Ads</a>
+							<hr>
+							<figure class="category row">
+
+								<div class="category-img col-md-3">
+									<img src="images/fr1.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="category-info">
-									<h4>Salles</h4>
-									<span>1,05,168 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
+								
+								<div class="category-img col-md-3">
+									<img src="images/fr6.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
 								</div>
-								<div class="clearfix"></div>
-							</div>
+								
+								<div class="category-img col-md-3">
+									<img src="images/fr13.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+
+								<div class="category-img col-md-3">
+									<img src="images/fr5.jpg" title="image" alt="" />
+									<figcaption>1,05,168 Ads <a class="btn btn-success right" href="3">Buy</a></figcaption>
+								</div>
+								
+								
+							</figure>
+
 							<div class="sub-categories">
 								<ul>
-									<li><a href="furnitures.php">Sofa & Dining</a></li>
-									<li><a href="furnitures.php">Beds & Wardrobes</a></li>
-									<li><a href="furnitures.php">Home Decor & Garden</a></li>
-									<li><a href="furnitures.php">Other Household Items</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Locations Complete</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Decorations</a></li>
+									<li class="btn btn-info"><a href="real-estate.php">Locations Simple</a></li>
 								</ul>
 							</div>
 						</div>
-						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat6.png" title="image" alt="" />
-								</div>
-								<div class="category-info">
-									<h4>Pets</h4>
-									<span>1,77,816 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="sub-categories">
-								<ul>
-									<li><a href="pets.php">Dogs</a></li>
-									<li><a href="pets.php">Aquariums</a></li>
-									<li><a href="pets.php">Pet Food & Accessories</a></li>
-									<li><a href="pets.php">Other Pets</a></li>
-								</ul>
-							</div>
-						</div>
-						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat7.png" title="image" alt="" />
-								</div>
-								<div class="category-info">
-									<h4>Books, Sports & Hobbies</h4>
-									<span>9,58,458 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="sub-categories">
-								<ul>
-									<li><a href="books-sports-hobbies.php">Books & Magazines</a></li>
-									<li><a href="books-sports-hobbies.php">Musical Instruments</a></li>
-									<li><a href="books-sports-hobbies.php">Sports Equipment</a></li>
-									<li><a href="books-sports-hobbies.php">Gym & Fitness</a></li>
-									<li><a href="books-sports-hobbies.php">Other Hobbies</a></li>
-								</ul>
-							</div>
-						</div>
-						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat8.png" title="image" alt="" />
-								</div>
-								<div class="category-info">
-									<h4>Fashion</h4>
-									<span>3,52,345 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="sub-categories">
-								<ul>
-									<li><a href="fashion.php">Clothes</a></li>
-									<li><a href="fashion.php">Footwear</a></li>
-									<li><a href="fashion.php">Accessories</a></li>
-								</ul>
-							</div>
-						</div>
-						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat9.png" title="image" alt="" />
-								</div>
-								<div class="category-info">
-									<h4>Kids</h4>
-									<span>8,45,298 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="sub-categories">
-								<ul>
-									<li><a href="kids.php">Furniture And Toys</a></li>
-									<li><a href="kids.php">Prams & Walkers</a></li>
-									<li><a href="kids.php">Accessories</a></li>
-								</ul>
-							</div>
-						</div>
-						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat10.png" title="image" alt="" />
-								</div>
-								<div class="category-info">
-									<h4>Services</h4>
-									<span>7,58,867 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="sub-categories">
-								<ul>
-									<li><a href="services.php">Education & Classes</a></li>
-									<li><a href="services.php">Web Development</a></li>
-									<li><a href="services.php">Electronics & Computer Repair</a></li>
-									<li><a href="services.php">Maids & Domestic Help</a></li>
-									<li><a href="services.php">Health & Beauty</a></li>
-									<li><a href="services.php">Movers & Packers</a></li>
-									<li><a href="services.php">Drivers & Taxi</a></li>
-									<li><a href="services.php">Event Services</a></li>
-									<li><a href="services.php">Other Services</a></li>
-								</ul>
-							</div>
-						</div>
-						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat11.png" title="image" alt="" />
-								</div>
-								<div class="category-info">
-									<h4>Jobs</h4>
-									<span>5,74,547 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="sub-categories">
-								<ul>
-									<li><a href="jobs.php">Customer Service</a></li>
-									<li><a href="jobs.php">IT</a></li>
-									<li><a href="jobs.php">Online</a></li>
-									<li><a href="jobs.php">Marketing</a></li>
-									<li><a href="jobs.php">Advertising & PR</a></li>
-									<li><a href="jobs.php">Sales</a></li>
-									<li><a href="jobs.php">Clerical & Administration</a></li>
-									<li><a href="jobs.php">Human Resources</a></li>
-									<li><a href="jobs.php">Education</a></li>
-									<li><a href="jobs.php">Hotels & Tourism</a></li>
-									<li><a href="jobs.php">Accounting & Finance</a></li>
-									<li><a href="jobs.php">Manufacturing</a></li>
-									<li><a href="jobs.php">Part - Time</a></li>
-									<li><a href="jobs.php">Other Jobs</a></li>
-								</ul>
-							</div>
-						</div>
-						<div>
-							<div class="category">
-								<div class="category-img">
-									<img src="images/cat12.png" title="image" alt="" />
-								</div>
-								<div class="category-info">
-									<h4>Real Estate</h4>
-									<span>98,156 Ads</span>
-									<a href="all-classifieds.php">View all Ads</a>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="sub-categories">
-								<ul>
-									<li><a href="real-estate.php">Houses</a></li>
-									<li><a href="real-estate.php">Apartments</a></li>
-									<li><a href="real-estate.php">PG & Roommates</a></li>
-									<li><a href="real-estate.php">Land & Plots</a></li>
-									<li><a href="real-estate.php">Shops - Offices - Commercial Space</a></li>
-									<li><a href="real-estate.php">Vacation Rentals - Guest Houses</a></li>
-								</ul>
-							</div>
-						</div>
+					</div>
+				</div>
+
 					</div>
 					<div class="clearfix"></div>
 				</div>
